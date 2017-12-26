ac73a831e85e8204e3f3b9664255ee509bc1c717
Ícaro Pinho Esmeraldo <icaropinhoe@gmail.com>
2017-12-26 13:56:56 -0200

    add packages structure

--- app/src/main/java/br/com/icaropinhoe/carros/activity/MainActivity.java
+++ app/src/main/java/br/com/icaropinhoe/carros/activity/MainActivity.java
@@ -1,7 +1,9 @@
-package br.com.icaropinhoe.carros;
+package br.com.icaropinhoe.carros.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import br.com.icaropinhoe.carros.R;
 
 public class MainActivity extends AppCompatActivity {
 
