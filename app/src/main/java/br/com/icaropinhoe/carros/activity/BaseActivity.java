d2b9bb2b9357ab618b389e7e82bbac5551efd867
Ícaro Pinho Esmeraldo <icaropinhoe@gmail.com>
2017-12-26 21:18:23 -0200

    Include toolbar

--- app/src/main/java/br/com/icaropinhoe/carros/activity/BaseActivity.java
+++ app/src/main/java/br/com/icaropinhoe/carros/activity/BaseActivity.java
@@ -1,10 +1,21 @@
 package br.com.icaropinhoe.carros.activity;
 
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+import br.com.icaropinhoe.carros.R;
 
 /**
  * Created by icaro on 26/12/2017.
  */
 
 public class BaseActivity extends AppCompatActivity {
+
+    protected void setUpToolbar(){
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        if(toolbar != null){
+            setSupportActionBar(toolbar);
+        }
+    }
+
 }
